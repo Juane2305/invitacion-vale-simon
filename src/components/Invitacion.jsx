688ee2ddb5a7bec6f6aa94bddb00ce9c13eac910
Juane <juane.elizondo23@gmail.com>
2025-09-01 18:44:06 -0300

    cambie song a public
    # Conflicts:
    #	public/song.mp3
    #	src/assets/song.MP3
    #	src/assets/song.mp3

--- src/components/Invitacion.jsx
+++ src/components/Invitacion.jsx
@@ -1,4 +1,4 @@
-import cancion from "../assets/song.mp3";
+import cancion from "/song.mp3";
 import Countdown from "./Countdown";
 import Lugares from "./Lugares";
 import GoogleCalendarButton from "./GoogleCalendarButton";
